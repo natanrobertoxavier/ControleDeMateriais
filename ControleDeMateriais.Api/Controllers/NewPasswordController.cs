891292bebaee4d5760369c93334bc6cf78fa4203
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2023-11-23 17:17:34 -0300

    remocao-variaveis-de-ambiente

--- ControleDeMateriais.Api/Controllers/NewPasswordController.cs
+++ ControleDeMateriais.Api/Controllers/NewPasswordController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeMateriais.Api.Controllers;
 
-[ServiceFilter(typeof(AuthenticatedUserAttribute))]
 public class NewPasswordController : ControleDeMateriaisController
 {
     [HttpPost]
